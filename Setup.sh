cff444ab64671d5f4177fe64b16f29804aff82c5
Nikhilanand <nikhilanand417@gmail.com>
2022-11-29 20:45:56 -0500

    Added file

--- Setup.sh
+++ Setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -e
 
-LOGFILE=/tmp/$COMPONENT.log
-
 COMPONENT=postgresql
 
 echo -n " Setting Up the Repo"
-sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm &>> $LOGFILE
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm 
 
 echo -n " Installing $COMPONENT "
-sudo yum install -y postgresql15-server &>> $LOGFILE
+sudo yum install -y postgresql15-server 
 
 echo -n " Optionally initialize the database and enable automatic start "
 sudo /usr/pgsql-15/bin/postgresql-15-setup initdb
